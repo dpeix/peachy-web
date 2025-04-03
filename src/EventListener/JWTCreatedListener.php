059e63a199ce014bd1446d43f206100790aa94b3
Dylan <dylanpeix@gmail.com>
2025-04-03 11:49:34 +0200

    Fixing EventListener

--- src/EventListener/JWTCreatedListener.php
+++ src/EventListener/JWTCreatedListener.php
@@ -1,25 +1,23 @@
+<?php
+
 namespace App\EventListener;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
-use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class JWTCreatedListener
 {
-    private RequestStack $requestStack;
-
-    public function __construct(RequestStack $requestStack)
-    {
-        $this->requestStack = $requestStack;
-    }
-
     public function onJWTCreated(JWTCreatedEvent $event): void
     {
-        $request = $this->requestStack->getCurrentRequest();
+        $user = $event->getUser();
 
-        if ($request && $user = $request->getUser()) {
-            $payload = $event->getData();
-            $payload['user_id'] = $user->getId(); // Assumes the User entity has a getId() method
-            $event->setData($payload);
+        if (!$user instanceof UserInterface) {
+            return;
         }
+
+        $payload = $event->getData();
+        $payload['id'] = $user->getId();
+
+        $event->setData($payload);
     }
 }
